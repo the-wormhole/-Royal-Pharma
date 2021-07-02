5c073727c1b9c40b446291f540f2af1a3e3228fa
Your Name <you@example.com>
2021-07-02 11:11:03 +0530

    Sign Up working

--- controllers/customercontroller.js
+++ controllers/customercontroller.js
@@ -1,6 +1,5 @@
 const Customer = require('../models/customer');
-const homeController = require('./homecontroller');
-
+//const homeController = require('./homecontroller');
 
 module.exports.signIn = function(req,res){
 
@@ -17,22 +16,36 @@ module.exports.signUp = function(req,res){
 }
 
 module.exports.create = function(req,res){
+     //console.log(req.body['confirm-password']);
+    if(req.body.password != req.body['confirm-password']){
+         return res.redirect('back');
+    }
 
-    //Karegey
-    console.log(req.body,);
-    Customer.create({
-        Name:req.body.Name,
-        Email:req.body.Email,
-        password:req.body.password
-    },function(err,newcustomer){
-        if(err){
-            console.log("Error in creating a new Customer!!",err);
-            // alert("Error in creating new Customer");
-            return;
-        }
-        console.log("**********",newcustomer);
-        return res.redirect('home');
-    });
+    Customer.findOne({Email:req.body.Email},function(err,customer){
+        if(err){console.log('Error in finding the user while signing Up!!')}
+
+        if(!customer){
+
+            Customer.create({
+                Name:req.body.Name,
+                Email:req.body.Email,
+                password:req.body.password
+            },function(err,newcustomer){
+                if(err){console.log("Error in creating a new Customer!!",err);return;}
+        
+                console.log("**********",newcustomer);
+                //return res.redirect('home');
+                // return res.render("customer_profile",{
+                //     customer_name:newcustomer.Name,
+                //     header: "Profile Page",
+                // });
+                return res.redirect('/customer/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        } 
+    })
+    
 }
 
 module.exports.createSession = function(req,res){
